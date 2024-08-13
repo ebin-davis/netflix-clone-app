4e2101156e6ad6b141d5b666350bb07b788a195b
ebi002 <126892942+ebi002@users.noreply.github.com>
2024-08-13 20:22:55 +0530

    changed gitignore file

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,3 +1,4 @@
-export const baseUrl='https://api.themoviedb.org/3';
-export const API_KEY="39887868e951ca6ec695f73c54f2c41b";
-export const imageUrl='https://image.tmdb.org/t/p/original'
+export const baseUrl = "https://api.themoviedb.org/3";
+export const API_KEY =import.meta.env.VITE_API_KEY;
+
+export const imageUrl = "https://image.tmdb.org/t/p/original";
